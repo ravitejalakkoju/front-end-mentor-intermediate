e742204e9d0f22fe2773270ff8917c06eb4cb4a2
Ravi Teja Lakkoju <raviteja6820@gmail.com>
2022-11-28 14:14:55 +0530

    feat(theme): added theme change functionality

---
+++ todo-app-main/theme-script.js
@@ -0,0 +1,41 @@
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+function loadThemeIcon(isLight = true) {
+  document.getElementById('js-theme-icon').setAttribute('src', isLight ? './images/icon-moon.svg' : './images/icon-sun.svg');
+}
+
+function loadSystemTheme() {
+  if (prefersDarkScheme.matches) {
+    loadThemeIcon(false);
+    document.body.classList.add("dark-theme");
+  } else {
+    loadThemeIcon();
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function loadDarkTheme() {
+  loadThemeIcon(false);
+  document.body.classList.add("dark-theme");
+}
+
+const themeToggle = document.getElementById("js-theme-toggle");
+const currentTheme = localStorage.getItem("theme");
+console.log(currentTheme);
+
+if(!currentTheme) loadSystemTheme();
+else if (currentTheme == "dark") loadDarkTheme();
+else loadThemeIcon();
+
+themeToggle.addEventListener("click", function() {
+  document.body.classList.toggle("dark-theme");
+  
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+
+  loadThemeIcon(theme == "light");
+
+  localStorage.setItem("theme", theme);
+});
