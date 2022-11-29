9f082a088678535cb5454393a3881f2d3e607d11
Ravi Teja Lakkoju <raviteja6820@gmail.com>
2022-11-29 18:07:02 +0530

    feat(backend): added backend with excel, test push

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions ={
    optionSuccessStatus:200,
 }
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
